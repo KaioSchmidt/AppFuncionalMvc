4d4b8d782d670fe016d005f9fde9ed648ce1a8eb
kaioschmidt1305 <kaio.schmidt@vidibr.com>
2022-06-01 19:04:01 -0300

    Add project files.

---
+++ AppMvc/Models/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace AppMvc.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Nome Completo")]
+        [Required(ErrorMessage = " O Campo {0} é obrigadtorio")]
+        public string Nome { get; set; }
+        public string Email  { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatorio")]
+        public string Cpf { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataMatricula  { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
